ec6bbc194dc9189de6d4da8f1163141b3600b01f
Lyadov Maxim <lyadov.maxim2018@yandex.ru>
2024-06-03 17:21:56 +0500

    fix: Исправил закрытие модального окна

--- src/app/indexComponents/time-table.component/timetable.component.ts
+++ src/app/indexComponents/time-table.component/timetable.component.ts
@@ -67,14 +67,17 @@ export class TimetableComponent {
   }
 
   public showDialog(employeeFio: string, recordingTime: string): void {
+    this.employeeTimeTableArr = this.localStorageService.getEmployeeTimeTableArr();
     for (let i = 0; i < this.employeeTimeTableArr.length; i++) {
       if (this.employeeTimeTableArr[i].fio == employeeFio) {
         for (let schedule of this.employeeTimeTableArr[i].schedules) {
           if (schedule.time == recordingTime) {
             if (schedule.isFree) {
+
               this.employeeFio = employeeFio;
               this.recordingTime = recordingTime;
               this.visibleMakeAppointment = true;
+              this.makeVisible1Mask();
             }
             else {
               this.employeeFio = employeeFio;
@@ -83,7 +86,9 @@ export class TimetableComponent {
               this.userOldFioForRedact = schedule.name;
               this.ageForRedact = schedule.age;
               this.genderForRedact = schedule.gender;
+
               this.visibleRedactAppointment = true;
+              this.makeVisible2Mask();
             }
           }
 
@@ -95,6 +100,7 @@ export class TimetableComponent {
   public saveAppointment(): void {
     if (this.userFio != "" && this.age != null && this.gender != '') {
       this.visibleMakeAppointment = false;
+      this.closeMask();
       this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Вы записались.' });
 
       let iWorker: IWorker = this.employeeTimeTableArr.find(worker => worker.fio === this.employeeFio);
@@ -105,7 +111,12 @@ export class TimetableComponent {
             if (schedule.time == this.recordingTime) {
               schedule.name = this.userFio;
               schedule.isFree = false;
+              schedule.age = this.age;
+              schedule.gender = this.gender;
               this.localStorageService.addRecordToLocaleStorage(this.employeeFio, this.userFio, this.date, this.recordingTime, this.age, this.gender);
+              this.userFio = "";
+              this.gender = "";
+              this.age = null;
             }
           }
         }
@@ -117,6 +128,7 @@ export class TimetableComponent {
 
   public saveChanges(): void {
     this.visibleRedactAppointment = false;
+    this.closeMask();
     this.localStorageService.saveChangesInLocalStorage(this.employeeFio, this.date, this.userFioForRedact, this.userOldFioForRedact, this.genderForRedact, this.ageForRedact, this.recordingTime);
     let iWorker: IWorker = this.employeeTimeTableArr.find(worker => worker.fio === this.employeeFio);
 
@@ -135,6 +147,7 @@ export class TimetableComponent {
 
   public deleteAppointment(): void {
     this.visibleRedactAppointment = false;
+    this.closeMask();
     this.localStorageService.deleteRecordFromLocalStorage(this.employeeFio, this.date, this.userFioForRedact);
     let iWorker: IWorker = this.employeeTimeTableArr.find(worker => worker.fio === this.employeeFio);
 
@@ -159,4 +172,32 @@ export class TimetableComponent {
     });
   }
 
+  public dialogClose(): void {
+    this.visibleMakeAppointment = false;
+    this.closeMask();
+  }
+
+  public closeMask(): void {
+    const overlayMasks = document.querySelectorAll<HTMLElement>('.p-dialog-mask');
+    // if (overlayMask) {
+    //   overlayMask.style.visibility = "hidden";
+    // }
+    overlayMasks.forEach(overlayMask => {
+      overlayMask.style.visibility = "hidden";
+    });
+  }
+
+  public makeVisible1Mask(): void {
+    const overlayMask = document.querySelectorAll<HTMLElement>('.p-dialog-mask');
+    if (overlayMask) {
+      overlayMask[0].style.visibility = "visible";
+    }
+  }
+
+  public makeVisible2Mask(): void {
+    const overlayMask = document.querySelectorAll<HTMLElement>('.p-dialog-mask');
+    if (overlayMask) {
+      overlayMask[1].style.visibility = "visible";
+    }
+  }
 }
